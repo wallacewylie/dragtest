b6e45a5656e4693f7763000a52bb7bfcde5207ef
wallacewylie <wallace.wylie@gmail.com>
2016-07-23 18:18:01 -0500

    Multiple Words

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $( function() {
     $( "#draggable" ).draggable();
 } );
+
+$( function() {
+    $( "#draggable2" ).draggable();
+} );
+
+$( function() {
+    $( "#draggable3" ).draggable();
+} );
+
+$( function() {
+    $( "#draggable4" ).draggable();
+} );
