4598607d517b719e5d3fde95e43038c2fb5d6db0
wallacewylie <wallace.wylie@gmail.com>
2016-07-23 17:09:26 -0500

    Working drag

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-var el = document.getElementById('target');
-var mover = false, x, y, posx, posy, first = true;
-el.onmousedown = function() {
-    mover = true;
-};
-el.onmouseup = function() {
-    mover = false;
-    first = true;
-};
-el.onmousemove = function(e) {
-    if (mover) {
-        if (first) {
-            x = e.offsetX;
-            y = e.offsetY;
-            first = false;
-        }
-        posx = e.pageX - x;
-        posy = e.pageY - y;
-        this.style.left = posx + 'px';
-        this.style.top = posy + 'px';
-    }
-};
+$( function() {
+    $( "#draggable" ).draggable();
+} );
